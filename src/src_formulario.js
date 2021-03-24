c0c03212fef99f8668ec98a03cdfeeed4bcd6399
Thiago Rodrigues <thi013022001@gmail.com>
2021-03-24 15:36:37 -0300

    escolher Img

--- src/src_formulario.js
+++ src/src_formulario.js
@@ -5,7 +5,8 @@ var botao = document.querySelector('body form#formulario button');
 botao.onclick = function abrirNovaGuia() {
     //metodo responsavel por abrir uma nova guia de acordo com os parametros.
     //nesse caso so estou pedindo que ele abra essa URL e como n especifiquei o segundo paramentro ja vai o _blank.
-    window.open('https://acesso.pagseguro.uol.com.br/?_ga=2.151002878.615896425.1588771743-416373785.1587232571');
+    window.open('index.html');
+    window.close();
 };
 
 //querySelector retorna o primeiro elemento dentro do body e do input.
